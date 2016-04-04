875093313f3fa07b242e73df72465c0ba280862c
Ben Bangert <ben@groovie.org>
2016-04-04 10:46:44 -0700

    feat: read processor settings from root of s3 dir
    Changes where the config is read from to the root of the S3 dir that
    is having its messages processed. This enables easier deployments in
    multi-deployed scenarios.
    
    Makefile updates to reduce the size of the generated lambda function
    substantially from ~28 MB to ~4 MB.
    
    Removed push_messages dependency as the single dynamodb call to make
    was more efficient to encode directly as part of the substantial
    reduction in lambda function package size.
    
    Closes #8

--- push_processor/processor/pubkey.py
+++ push_processor/processor/pubkey.py
@@ -4,11 +4,15 @@ from collections import defaultdict, deque
 class PubKeyProcessor(object):
     def __init__(self, pubkey_list):
         # Store public keys as hash for fast lookup
+        self.total = 0
+        self.flagged = 0
         self.pubkeys = {k: True for k in pubkey_list}
         self.latest_messages = defaultdict(lambda: deque(maxlen=100))
 
     def process_message(self, message):
         """Process a message and store metadata for top matches"""
+        self.total += 1
+
         # Need a message field and jwt
         if "message" not in message.fields or "jwt" not in message.fields:
             return
@@ -25,4 +29,5 @@ class PubKeyProcessor(object):
         if message_key not in self.pubkeys:
             return
 
+        self.flagged += 1
         self.latest_messages[message_key].append(message)
